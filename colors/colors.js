22a9b12eb90989dd56db9c7f38d5319ed6413661
Vellyus <noisyboy026@gmail.com>
2019-07-19 19:26:54 +0200

    added colors + new blog post

---
+++ colors/colors.js
@@ -0,0 +1,28 @@
+var html = '';
+var rgbColor;
+
+function randomRGB() {
+    return Math.floor(Math.random() * 256);
+}
+
+function randomColor() {
+    var color = 'rgb('
+    color += randomRGB() + ',';
+    color += randomRGB() + ',';
+    color += randomRGB() + ')';
+    return color;
+}
+
+function print(message) {
+    document.write(message);
+}
+
+for (var i = 0; i < 10000; i += 1) {
+    red = randomRGB();
+    green = randomRGB();
+    blue = randomRGB();
+    rgbColor = randomColor();
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+}
+
+print(html);
