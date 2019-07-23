f583b3440a8005eea9c1dd0652f3130d4d5136d3
Vellyus <noisyboy026@gmail.com>
2019-07-23 16:14:13 +0200

    Refactored petals.js , uploaded the original one on github and wrote a blog post about it

--- petals/petals.js
+++ petals/petals.js
@@ -1,135 +1,56 @@
-/*
-Felépítés
-
-Dobókocka - Math.floor(Math.random() *6) +1 -> DONE
-A dobókocka értékéhez megfelelő képeket rendelünk -> DONE
-A dobókockából kinyert értéket konvertáljuk a játéknak megfelelően és tároljuk -> 1,2,4,6 = 0 / 3 = 2 / 5 = 4  -> DONE
-Összeadjuk a konvertált értékeket és közöljuk a játékossal a dobás eredményét
-
-*/
-
-//Creates a die roll
-var dieRoll1 = Math.floor(Math.random() * 6) + 1;
-var dieRoll2 = Math.floor(Math.random() * 6) + 1;
-var dieRoll3 = Math.floor(Math.random() * 6) + 1;
-var dieRoll4 = Math.floor(Math.random() * 6) + 1;
-var dieRoll5 = Math.floor(Math.random() * 6) + 1;
-
-
-//Displays a picture for the dieRoll1
-if (dieRoll1 === 1) {
-  document.write('<img src="../img/die1.jpg">');
-} else if (dieRoll1 === 2) {
-  document.write('<img src="../img/die2.jpg">');
-} else if (dieRoll1 === 3) {
-  document.write('<img src="../img/die3.jpg">');
-} else if (dieRoll1 === 4) {
-  document.write('<img src="../img/die4.jpg">');
-} else if (dieRoll1 === 5) {
-  document.write('<img src="../img/die5.jpg">');
-} else {
-  document.write('<img src="../img/die6.jpg">');
+function dieRoll() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-//Displays a picture for the dieRoll2
-if (dieRoll2 === 1) {
-  document.write('<img src="../img/die1.jpg">');
-} else if (dieRoll2 === 2) {
-  document.write('<img src="../img/die2.jpg">');
-} else if (dieRoll2 === 3) {
-  document.write('<img src="../img/die3.jpg">');
-} else if (dieRoll2 === 4) {
-  document.write('<img src="../img/die4.jpg">');
-} else if (dieRoll2 === 5) {
-  document.write('<img src="../img/die5.jpg">');
-} else {
-  document.write('<img src="../img/die6.jpg">');
+function print(message) {
+  document.write(message);
 }
 
-//Displays a picture for the dieRoll3
-if (dieRoll3 === 1) {
-  document.write('<img src="../img/die1.jpg">');
-} else if (dieRoll3 === 2) {
-  document.write('<img src="../img/die2.jpg">');
-} else if (dieRoll3 === 3) {
-  document.write('<img src="../img/die3.jpg">');
-} else if (dieRoll3 === 4) {
-  document.write('<img src="../img/die4.jpg">');
-} else if (dieRoll3 === 5) {
-  document.write('<img src="../img/die5.jpg">');
-} else {
-  document.write('<img src="../img/die6.jpg">');
+function displayPicture(dieRollNr) {
+  if (dieRollNr === 1) {
+    print('<img src="../img/die1.jpg">')
+  } else if (dieRollNr === 2) {
+    print('<img src="../img/die2.jpg">')
+  } else if (dieRollNr === 3) {
+    print('<img src="../img/die3.jpg">')
+  } else if (dieRollNr === 4) {
+    print('<img src="../img/die4.jpg">')
+  } else if (dieRollNr === 5) {
+    print('<img src="../img/die5.jpg">')
+  } else {
+    print('<img src="../img/die6.jpg">')
+  }
+
 }
 
-//Displays a picture for the dieRoll4
-if (dieRoll4 === 1) {
-  document.write('<img src="../img/die1.jpg">');
-} else if (dieRoll4 === 2) {
-  document.write('<img src="../img/die2.jpg">');
-} else if (dieRoll4 === 3) {
-  document.write('<img src="../img/die3.jpg">');
-} else if (dieRoll4 === 4) {
-  document.write('<img src="../img/die4.jpg">');
-} else if (dieRoll4 === 5) {
-  document.write('<img src="../img/die5.jpg">');
-} else {
-  document.write('<img src="../img/die6.jpg">');
+function convertValue(dieRollNr) {
+  var dieValue = 0;
+  if (dieRollNr === 3) {
+    dieValue += 2;
+  } else if (dieRollNr === 5) {
+    dieValue += 4;
+  }
+  return dieValue;
 }
 
-//Displays a picture for the dieRoll5
-if (dieRoll5 === 1) {
-  document.write('<img src="../img/die1.jpg">');
-} else if (dieRoll5 === 2) {
-  document.write('<img src="../img/die2.jpg">');
-} else if (dieRoll5 === 3) {
-  document.write('<img src="../img/die3.jpg">');
-} else if (dieRoll5 === 4) {
-  document.write('<img src="../img/die4.jpg">');
-} else if (dieRoll5 === 5) {
-  document.write('<img src="../img/die5.jpg">');
-} else {
-  document.write('<img src="../img/die6.jpg">');
-}
+var dieRoll1 = dieRoll();
+var dieRoll2 = dieRoll();
+var dieRoll3 = dieRoll();
+var dieRoll4 = dieRoll();
+var dieRoll5 = dieRoll();
 
+var value1 = convertValue(dieRoll1);
+var value2 = convertValue(dieRoll2);
+var value3 = convertValue(dieRoll3);
+var value4 = convertValue(dieRoll4);
+var value5 = convertValue(dieRoll5);
 
-// konvertálja a dieRoll-ok értékeit a játéknak megfelelően
-var petals1 = 0;
-var petals2 = 0;
-var petals3 = 0;
-var petals4 = 0;
-var petals5 = 0;
+displayPicture(dieRoll1);
+displayPicture(dieRoll2);
+displayPicture(dieRoll3);
+displayPicture(dieRoll4);
+displayPicture(dieRoll5);
 
-if (dieRoll1 === 3) {
-  petals1 += 2;
-} else if (dieRoll1 === 5) {
-  petals1 += 4;
-}
-
-if (dieRoll2 === 3) {
-  petals2 += 2;
-} else if (dieRoll2 === 5) {
-  petals2 += 4;
-}
-
-if (dieRoll3 === 3) {
-  petals3 += 2;
-} else if (dieRoll3 === 5) {
-  petals3 += 4;
-}
-
-if (dieRoll4 === 3) {
-  petals4 += 2;
-} else if (dieRoll4 === 5) {
-  petals4 += 4;
-}
-
-if (dieRoll5 === 3) {
-  petals5 += 2;
-} else if (dieRoll5 === 5) {
-  petals5 += 4;
-}
-
-
-var result = petals1 + petals2 + petals3 + petals4 + petals5;
-document.write("<br>");
-document.write("The result of this roll is " + result);
+var result = value1 + value2 + value3 + value4 + value5;
+print("<br>");
+print("The result of this roll is " + result);
