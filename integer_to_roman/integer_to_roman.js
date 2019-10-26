fa9f7a122bef71d9906f15a6592b7ead6fe26cff
Vellyus <noisyboy026@gmail.com>
2019-10-26 14:17:02 +0200

    Added "integer to roman" + new blog post

---
+++ integer_to_roman/integer_to_roman.js
@@ -0,0 +1,12 @@
+const numerals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const romanNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+let input = parseInt(prompt("Type in a number!"));
+let i = 0;
+
+do {
+    for (i; input - numerals[i] < 0; i++) {
+        console.log(i);
+    }
+    input -= numerals[i];
+    document.write(romanNumerals[i]);
+} while (input > 0)
