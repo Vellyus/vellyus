bc381c6937cc33d9695dde9febb7af813c3dfd55
Vellyus <noisyboy026@gmail.com>
2020-12-07 12:31:37 +0100

    moved assets to their own project's folder

--- four_in_a_row/js/Board.js
+++ four_in_a_row/js/Board.js
@@ -1,5 +1,7 @@
-class Board {
-  constructor() {
+class Board
+{
+  constructor()
+  {
     this.rows = 6;
     this.columns = 7;
     this.spaces = this.createSpaces();
@@ -13,13 +15,16 @@ class Board {
    * @return  {Array}     An array of space objects
    */
 
-  createSpaces() {
+  createSpaces()
+  {
     const spaces = [];
 
-    for (let x = 0; x < this.columns; x++) {
+    for (let x = 0; x < this.columns; x++)
+    {
       const column = [];
 
-      for (let y = 0; y < this.rows; y++) {
+      for (let y = 0; y < this.rows; y++)
+      {
         const space = new Space(x, y);
         column.push(space);
       }
@@ -29,33 +34,37 @@ class Board {
     return spaces;
   }
 
-  drawHTMLBoard() {
-    for (let column of this.spaces) {
-      for (let space of column) {
+  drawHTMLBoard()
+  {
+    for (let column of this.spaces)
+    {
+      for (let space of column)
+      {
         space.drawSVGSpace();
       }
     }
   }
 
-  addArrows() {
+  addArrows()
+  {
     this.leftArrow.setAttribute('class', 'arrow');
     this.leftArrow.setAttribute('id', 'leftArrow');
     const leftImg = document.createElement('img');
-    leftImg.setAttribute('src', '../../assets/circle_left.svg');
+    leftImg.setAttribute('src', 'img//circle_left.svg');
     document.querySelector('.table').appendChild(this.leftArrow).appendChild(leftImg);
     this.leftArrow.addEventListener('click', () => { game.activePlayer.activeToken.moveLeft() });
 
     this.rightArrow.setAttribute('class', 'arrow');
     this.rightArrow.setAttribute('id', 'rightArrow');
     const rightImg = document.createElement('img');
-    rightImg.setAttribute('src', '../../assets/circle_right.svg');
+    rightImg.setAttribute('src', 'img//circle_right.svg');
     document.querySelector('.table').appendChild(this.rightArrow).appendChild(rightImg);
     this.rightArrow.addEventListener('click', () => { game.activePlayer.activeToken.moveRight(game.board.columns); })
 
     this.downArrow.setAttribute('class', 'arrow');
     this.downArrow.setAttribute('id', 'downArrow');
     const downImg = document.createElement('img');
-    downImg.setAttribute('src', '../../assets/circle_down.svg');
+    downImg.setAttribute('src', 'img//circle_down.svg');
     document.querySelector('.table').appendChild(this.downArrow).appendChild(downImg);
     this.downArrow.addEventListener('click', () => { game.playToken() });
 
